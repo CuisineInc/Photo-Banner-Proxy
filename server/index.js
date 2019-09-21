a8a550a7159df6c75650e084af94fb43959c8946
Soro Didiouf <ssoro013@gmail.com>
2019-09-21 12:23:12 -0700

    Stress Test

--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
+require('newrelic');
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const bodyParser = require('body-parser');
-// const db = require('../database/index.js');
 const path = require('path');
-
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname,  '../public')));
-app.use('/:restaurantName/', express.static(path.join(__dirname,  '../public')));
+
+app.use('/:restaurant/', express.static(path.join(__dirname,  '../public')));
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(cors())
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 app.listen(PORT, function() {
-  console.log(`listening on port ${PORT}`);
+  console.log(`App is listening on port ${PORT}!`);
 });
